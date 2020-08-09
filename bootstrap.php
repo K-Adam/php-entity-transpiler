e98a7b234f65655c87bdb3645e8f07b654ee4c52
Adam Kecskes <kecskes.adam@outlook.com>
2020-08-09 23:43:18 +0200

    Register annotations

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$anRoot = __DIR__."/src/Annotations/";
+
+AnnotationRegistry::registerFile($anRoot."Entity.php");
+AnnotationRegistry::registerFile($anRoot."Property.php");
+AnnotationRegistry::registerFile($anRoot."Collection.php");
+AnnotationRegistry::registerFile($anRoot."Map.php");
